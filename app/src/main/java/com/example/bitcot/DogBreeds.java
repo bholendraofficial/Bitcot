96ecaf07d1e5d862b76fe45cf483dc1d354560b7
Bholendra Singh <47040352+bholendraofficial@users.noreply.github.com>
2020-02-29 19:52:48 +0530

    fix

---
+++ app/src/main/java/com/example/bitcot/DogBreeds.java
@@ -0,0 +1,4 @@
+package com.example.bitcot;
+
+public class DogBreeds {
+}
--- app/src/main/java/com/example/bitcot/MainActivity.java
+++ app/src/main/java/com/example/bitcot/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.bitcot;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findIDS();
+    }
+
+    private void findIDS() {
+        recyclerView=(RecyclerView)findViewById(R.id.recyclerView);
     }
 }
